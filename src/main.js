af6e4570319c46ab26b7bb5d949c758f7430970b
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-22 16:07:41 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,76 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import err from './img/err.png';
+import httpRequest from './js/pixabay-api.js';
+import createMarkup from './js/render-functions.js';
+
+const key = '44447356-a60fa6f4c2d7f10e895940a18';
+const form = document.querySelector('.form');
+const list = document.querySelector('.list');
+
+form.addEventListener('submit', searchHandler);
+
+function searchHandler(evt) {
+  list.innerHTML = '';
+  const text = evt.target.elements.input.value.trim();
+  evt.preventDefault();
+  if (text != 0) {
+    form.insertAdjacentHTML('afterend', '<span class="loader"></span>');
+    const loader = document.querySelector('.loader');
+    httpRequest(key, text)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const photos = data.hits;
+        if (photos.length !== 0) {
+          list.insertAdjacentHTML('beforeend', createMarkup(photos));
+          const lightbox = new SimpleLightbox('.list a', {
+            captions: true,
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250,
+          });
+          lightbox.refresh();
+        } else {
+          iziToast.show({
+            class: 'toast',
+            position: 'topRight',
+            icon: 'icon',
+            iconUrl: err,
+            iconColor: 'white',
+            messageColor: 'white',
+            message: `Sorry, there are no images matching your search query. Please try again!`,
+          });
+        }
+      })
+      .catch(error => {
+        iziToast.show({
+          class: 'toast',
+          position: 'topRight',
+          icon: 'icon',
+          iconUrl: err,
+          iconColor: 'white',
+          messageColor: 'white',
+          title: 'Error',
+          titleColor: 'white',
+          message: `Please try again!`,
+        });
+        if (error.response) {
+          console.error('Server error:', error.response.status);
+        } else if (error.request) {
+          console.error('No response from server');
+        } else {
+          console.error('Unknown error:', error.message);
+        }
+      })
+      .finally(() => (loader.style.display = 'none'));
+    form.reset();
+  }
+}
